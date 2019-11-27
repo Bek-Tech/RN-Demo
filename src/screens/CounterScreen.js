18d1de5850719e6014fb0c3badb69781742a34e1
bekwdev <bekwdev@gmail.com>
2019-11-27 13:13:01 -0500

    in Counter Demo useState deleted and changed to useReducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,9 +1,25 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
+const reducer = (state, action) => {
+  // state === 0
+  //  action ==== {  type: "increase"   "decrease"      payload :  1, -1}
+  switch (action.type) {
+    case 'increase':
+      return state + action.payload;
+    case 'decrease':
+      return state + -1 * action.payload;
+
+    default:
+      break;
+  }
+};
+
 const CounterScreen = () => {
-  //use this
-  const [count, setCount] = useState (0);
+  //   Reducer
+  const [state, dispatch] = useReducer (reducer, 0);
+  //useState
+  // const [count, setCount] = useState (0);
   //Do not do that
   //   let count = 0;
   return (
@@ -11,24 +27,30 @@ const CounterScreen = () => {
       <Button
         title="Increase"
         onPress={() => {
+          //  useReducer
+
+          dispatch ({type: 'increase', payload: 1});
           //do not use
           // count++;
 
-          //  use this
-          setCount (count + 1);
+          //  useState
+          // setCount (count + 1);
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
+          //   useReducer
+
+          dispatch ({type: 'decrease', payload: 1});
           //do not use
           //count--;
 
-          // use this
-          setCount (count - 1);
+          // useState
+          // setCount (count - 1);
         }}
       />
-      <Text>Counted Number: {count}</Text>
+      <Text>Counted Number: {state}</Text>
     </View>
   );
 };
